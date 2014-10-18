799493d4c748ed893ccb165bb2806d51fa592bde
gudrun inga <gudrun@i-d979896b.greenqloud.internal>
2014-10-18 15:37:24 +0000

    Refactored test of multiple numbers or arguments

--- stringcalculator/src/main/java/is/ru/stringcalculator/Calculator.java
+++ stringcalculator/src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,12 +6,7 @@ public class Calculator {
 		if(text.equals(""))	
 			return 0;
 		else if (text.contains(",")){
-			String[] numbers = text.split(",");
-			int total = 0;
-			for(String number : numbers){
-				total += toInt(number);
-			}
-			return total;
+			return sum(splitNumbers(text));
 		}
 		else 
 			return 1;
@@ -20,4 +15,16 @@ public class Calculator {
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}	
+
+	private static String[] splitNumbers(String numbers){
+		return numbers.split(",");
+	}
+
+	private static int sum(String[] numbers){
+		int total = 0;
+		for(String number : numbers){
+			total += toInt(number);
+		}
+		return total;
+	}
 }
